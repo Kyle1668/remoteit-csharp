f222fd078ab987e48911e3ca06aa391fe7edf189
Kyle1668 <kyledevinobrien1@gmail.com>
2020-04-09 20:23:49 -0700

    added RemoteitClient class

---
+++ Remoteit/RemoteitClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Remoteit
+{
+    public class RemoteitClient
+    {
+        public RemoteitClient(string developerKey, string userName, string password)
+        {
+        }
+    }
+}
