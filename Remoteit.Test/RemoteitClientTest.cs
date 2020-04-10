9013a20e888f8fcc4acdb61ca5bbc54bd3c07d75
Kyle1668 <kyledevinobrien1@gmail.com>
2020-04-09 21:52:37 -0700

    refactored unti tests for consutrctor

--- Remoteit.Test/RemoteitClientTest.cs
+++ Remoteit.Test/RemoteitClientTest.cs
@@ -5,18 +5,14 @@ namespace Remoteit.Test
 {
     public class RemoteitClientTest
     {
-        public RemoteitClientTest()
-        {
-        }
-
         [Fact]
         public void AuthenticateSuccessfully()
         {
             // Mock 200 Response from API. Should not throw excpetion.
-            var r3Client = new RemoteitClient("c9xNsPKT7Yo5xHNj", "kyle1668", "foo");
+            var testClient = new RemoteitClient("c9xNsPKT7Yo5xHNj", "kyle1668", "foo");
 
-            Assert.Equal(RemoteitClient.DeveloperKey, "c9xNsPKT7Yo5xHNj");
-            Assert.Equal(RemoteitClient.UserName, "kyle1668");
+            Assert.Equal("c9xNsPKT7Yo5xHNj", testClient.DeveloperKey);
+            Assert.Equal("kyle1668", testClient.UserName);
         }
 
         [Fact]
