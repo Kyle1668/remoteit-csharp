8381fdb492bcc2a92cc00b1f9cbdc1029dad495b
Kyle1668 <kyledevinobrien1@gmail.com>
2020-04-09 21:53:24 -0700

    Began developing method for sending requests to the login endpoint

--- Remoteit/RemoteitClient.cs
+++ Remoteit/RemoteitClient.cs
@@ -1,13 +1,57 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Remoteit.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Collections.Generic;
-using System.Text;
+using System.Security.Authentication;
 
 namespace Remoteit
 {
     public class RemoteitClient
     {
-        public RemoteitClient(string developerKey, string userName, string password)
+        private static readonly HttpClient _httpApiClient = new HttpClient()
         {
+            BaseAddress = new System.Uri("https://api.remot3.it/apv/v27/")
+        };
+
+        public readonly IEnumerable<char> DeveloperKey;
+
+        public readonly IEnumerable<char> UserName;
+
+        private readonly IEnumerable<char> _userPassword;
+
+        private RemoteitApiSession _currentSession;
+
+        public RemoteitClient(IEnumerable<char> developerKey, IEnumerable<char> userName, IEnumerable<char> password)
+        {
+            DeveloperKey = developerKey;
+            UserName = userName;
+            _userPassword = password;
+            _httpApiClient.DefaultRequestHeaders.Add("developerkey", developerKey.ToString());
+        }
+
+        private async Task<RemoteitApiSession> AuthenticateToApi()
+        {
+            var apiEndpoint = string.Concat(_httpApiClient.BaseAddress, "device/connect");
+
+            var requestBody = new Dictionary<string, IEnumerable<char>>()
+            {
+                { "username", UserName },
+                { "password", _userPassword }
+            };
+
+            var rawJsonRequestBody = new StringContent(JsonConvert.SerializeObject(requestBody));
+
+            try
+            {
+                HttpResponseMessage response = await _httpApiClient.PostAsync(apiEndpoint, rawJsonRequestBody);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<RemoteitApiSession>(responseBody);
+            }
+            catch (HttpRequestException apiRequestError)
+            {
+                throw new AuthenticationException(apiRequestError.Message);
+            }
         }
     }
 }
