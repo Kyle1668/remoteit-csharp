469823db34a8f480ffc1b593ac35b59cf15320c1
Kyle1668 <kyledevinobrien1@gmail.com>
2020-04-09 22:48:41 -0700

    Removed depricated tests

--- Remoteit.Test/RemoteitClientTest.cs
+++ Remoteit.Test/RemoteitClientTest.cs
@@ -8,31 +8,10 @@ namespace Remoteit.Test
         [Fact]
         public void AuthenticateSuccessfully()
         {
-            // Mock 200 Response from API. Should not throw excpetion.
             var testClient = new RemoteitClient("c9xNsPKT7Yo5xHNj", "kyle1668", "foo");
 
             Assert.Equal("c9xNsPKT7Yo5xHNj", testClient.DeveloperKey);
             Assert.Equal("kyle1668", testClient.UserName);
-        }
-
-        [Fact]
-        public void ThrowExcpetionWhenAuthenticationFails()
-        {
-            Assert.Throws<AuthenticationException>(() =>
-            {
-                // Mock 401 Response from API
-                new RemoteitClient("foo", "foo", "foo");
-            });
-        }
-
-        [Fact]
-        public void ThrowExceptionWhenUnableToReachAPI()
-        {
-            Assert.Throws<AuthenticationException>(() =>
-            {
-                // Mock 401 Response from API
-                new RemoteitClient("foo", "foo", "foo");
-            });
         }
     }
 }
