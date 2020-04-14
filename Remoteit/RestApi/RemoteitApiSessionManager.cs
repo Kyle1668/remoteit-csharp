aa0232d4a214670fa751fc2367f30e3d011629c8
Kyle1668 <kyledevinobrien1@gmail.com>
2020-04-13 17:23:43 -0700

    Added a GenerateSession method to RemoteitApiSessionManager

--- Remoteit/RestApi/RemoteitApiSessionManager.cs
+++ Remoteit/RestApi/RemoteitApiSessionManager.cs
@@ -1,8 +1,10 @@
-﻿using Remoteit.Util;
-using System;
+﻿using Remoteit.Models;
+using Remoteit.Util;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
-using System.Text;
+using System.Security.Authentication;
+using System.Text.Json;
 
 [assembly: InternalsVisibleTo("Remoteit.Test")]
 
@@ -12,16 +14,42 @@ namespace Remoteit.RestApi
     {
         private IUnixTimeStampCalculator _timeCalculator;
 
+        private HttpClient _httpApiClient { get; }
+
         public RemoteitApiSession CurrentSessionData { get; set; }
 
-        public RemoteitApiSessionManager(IUnixTimeStampCalculator timeCalculator)
+        public RemoteitApiSessionManager(IUnixTimeStampCalculator timeCalculator, HttpClient httpClient)
         {
             _timeCalculator = timeCalculator;
+            _httpApiClient = httpClient;
+        }
+
+        public async void GenerateSession(string userName, string userPassword)
+        {
+            var apiEndpoint = string.Concat(_httpApiClient.BaseAddress, "device/connect");
+
+            var requestBody = new Dictionary<string, IEnumerable<char>>()
+            {
+                { "username", userName },
+                { "password", userPassword }
+            };
+
+            var rawJsonRequestBody = new StringContent(JsonSerializer.Serialize(requestBody));
+
+            try
+            {
+                HttpResponseMessage response = await _httpApiClient.PostAsync(apiEndpoint, rawJsonRequestBody);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                CurrentSessionData = JsonSerializer.Deserialize<RemoteitApiSession>(responseBody);
+            }
+            catch (HttpRequestException apiRequestError)
+            {
+                throw new AuthenticationException(apiRequestError.Message);
+            }
         }
 
         public bool SessionHasExpired()
         {
-            //return TokenExpirationDate <= DateTimeOffset.Now.ToUnixTimeMilliseconds();
             return CurrentSessionData.TokenExpirationDate <= _timeCalculator.Calculate();
         }
     }
