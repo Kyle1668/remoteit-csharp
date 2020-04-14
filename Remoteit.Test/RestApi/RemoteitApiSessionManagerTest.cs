ecb1bb20baf787eca2bae289965e2a0c748c85d0
Kyle1668 <kyledevinobrien1@gmail.com>
2020-04-13 17:22:34 -0700

    added unit test for an exceptiong being thrown when there is no API session

--- Remoteit.Test/RestApi/RemoteitApiSessionManagerTest.cs
+++ Remoteit.Test/RestApi/RemoteitApiSessionManagerTest.cs
@@ -2,6 +2,8 @@
 using Moq;
 using Remoteit.RestApi;
 using Remoteit.Util;
+using System.Net.Http;
+using System;
 
 namespace Remoteit.Test.RestApi
 {
@@ -9,13 +11,25 @@ namespace Remoteit.Test.RestApi
     {
         private readonly string testDevKey = "XXXXXX";
 
+        // Test ToDo: Throw an exception if the current session is null.
+        [Fact]
+        public void TestExceptionThrownWhenCheckingExpirationWithNoSession()
+        {
+            var testSession = new RemoteitApiSessionManager(new UnixTimeStampCalculator(), new HttpClient());
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                testSession.SessionHasExpired();
+            });
+        }
+
         [Fact]
         public void TestSessionExpiredCalculation()
         {
             var mockTimer = new Mock<IUnixTimeStampCalculator>();
             mockTimer.Setup(x => x.Calculate()).Returns(1587257611);
 
-            var testSession = new RemoteitApiSessionManager(mockTimer.Object);
+            var testSession = new RemoteitApiSessionManager(mockTimer.Object, new HttpClient());
             testSession.CurrentSessionData.TokenExpirationDate = 1587100798;
 
             Assert.True(testSession.SessionHasExpired());
@@ -27,10 +41,25 @@ namespace Remoteit.Test.RestApi
             var mockTimer = new Mock<IUnixTimeStampCalculator>();
             mockTimer.Setup(x => x.Calculate()).Returns(1587100798);
 
-            var testSession = new RemoteitApiSessionManager(mockTimer.Object);
+            var testSession = new RemoteitApiSessionManager(mockTimer.Object, new HttpClient());
             testSession.CurrentSessionData.TokenExpirationDate = 1587257611;
 
             Assert.False(testSession.SessionHasExpired());
+        }
+
+        [Fact]
+        public void TestCreatingANewApiSession()
+        {
+            // Test the following
+            // 1. That the http request is sent once with the correct arguments.
+            // 2. That CurrentSessionData has changed. Lazy way could be to check if the token is different?
+        }
+
+        [Fact]
+        public void TestHandlingUnableToCreateNewSession()
+        {
+            // Test the following
+            // 1. If the API returns a non-200 response, throw an Unable to authenticate exception.
         }
     }
 }
