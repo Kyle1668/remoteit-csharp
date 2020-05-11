a7161f4347c970382108cc90ea87f80423b5eb2f
Kyle1668 <kyledevinobrien1@gmail.com>
2020-05-11 14:02:37 -0700

    refactored RemoteitClient

--- Remoteit/RemoteitClient.cs
+++ Remoteit/RemoteitClient.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
-using System.Security.Authentication;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -18,27 +17,6 @@ namespace Remoteit
     /// </summary>
     public class RemoteitClient : IRemoteitClient
     {
-        /// <summary>
-        /// The Http client used to facilitate requests to the remote.it REST API.
-        /// </summary>
-        private HttpClient _httpApiClient { get; }
-
-        /// <summary>
-        /// Required for authentication. Your developer key which can be found by logging into remote.it and going to your Account settings page.
-        /// </summary>
-        public string DeveloperKey { get; }
-
-        private string _userName { get; }
-
-        private string _userPassword { get; }
-
-        internal IRemoteitApiSessionManager CurrentSession { get; set; }
-
-        private bool _isInvalidSession
-        {
-            get { return CurrentSession == null || CurrentSession.SessionHasExpired(); }
-        }
-
         public RemoteitClient(string userName, string password, string developerKey, HttpClient requestClient = null)
         {
             _userName = userName;
@@ -49,25 +27,23 @@ namespace Remoteit
             CurrentSession = new RemoteitApiSessionManager(new UnixTimeStampCalculator(), _httpApiClient);
         }
 
-        public async Task<List<RemoteitDevice>> GetDevices()
+        /// <summary>
+        /// Required for authentication. Your developer key which can be found by logging into remote.it and going to your Account settings page.
+        /// </summary>
+        public string DeveloperKey { get; }
+
+        internal IRemoteitApiSessionManager CurrentSession { get; set; }
+
+        private HttpClient _httpApiClient { get; }
+
+        private bool _isInvalidSession
         {
-            if (_isInvalidSession)
-            {
-                CurrentSession.CurrentSessionData = await CurrentSession.GenerateSession(_userName, _userPassword, DeveloperKey);
-            }
-
-            var httpRequest = new HttpRequestMessage()
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri(string.Concat(_httpApiClient.BaseAddress, "/device/list/all"))
-            };
-            httpRequest.Headers.Add("developerkey", DeveloperKey.ToString());
-            httpRequest.Headers.Add("token", CurrentSession.CurrentSessionData.Token);
-
-            var apiRequestSender = new RemoteitApiRequest<DevicesListEndpointResponse>();
-            DevicesListEndpointResponse results = await apiRequestSender.SendAsync(_httpApiClient, httpRequest);
-            return results.Devices;
+            get { return CurrentSession == null || CurrentSession.SessionHasExpired(); }
         }
+
+        private string _userName { get; }
+
+        private string _userPassword { get; }
 
         public async Task<ServiceConnection> ConnectToService(string deviceAddress)
         {
@@ -94,6 +70,26 @@ namespace Remoteit
             var apiRequestSender = new RemoteitApiRequest<ServiceConnectionEndpointResponse>();
             ServiceConnectionEndpointResponse results = await apiRequestSender.SendAsync(_httpApiClient, httpRequest);
             return results.Connection;
+        }
+
+        public async Task<List<RemoteitDevice>> GetDevices()
+        {
+            if (_isInvalidSession)
+            {
+                CurrentSession.CurrentSessionData = await CurrentSession.GenerateSession(_userName, _userPassword, DeveloperKey);
+            }
+
+            var httpRequest = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(string.Concat(_httpApiClient.BaseAddress, "/device/list/all"))
+            };
+            httpRequest.Headers.Add("developerkey", DeveloperKey.ToString());
+            httpRequest.Headers.Add("token", CurrentSession.CurrentSessionData.Token);
+
+            var apiRequestSender = new RemoteitApiRequest<DevicesListEndpointResponse>();
+            DevicesListEndpointResponse results = await apiRequestSender.SendAsync(_httpApiClient, httpRequest);
+            return results.Devices;
         }
     }
 }
