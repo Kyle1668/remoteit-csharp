f2a382afbaa1f8298135757ec66d72727ecc1091
Kyle1668 <kyledevinobrien1@gmail.com>
2020-05-17 14:54:00 -0700

    Refactored RemoteitApiSessionManagerTest

--- Remoteit.Test/RemoteitClientTest.cs
+++ Remoteit.Test/RemoteitClientTest.cs
@@ -6,7 +6,6 @@ using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
